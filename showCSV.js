b0c34a7d33ff9b122c8ad0aa4c189e2b5ecf7890
Sancler <sanclerprodigy@gmail.com>
2024-09-03 14:37:55 -0300

    substitui funçoes por ...

--- showCSV.js
+++ showCSV.js
@@ -1,6 +1,8 @@
 //// Função Principal
 const fs = require('fs');
 const csv = fs.readFileSync('./oscar_best_pictures.csv').toString();
+const {quebraLinhas, separaColunas, separaConteudo, constroiListaDeObjetos
+} = require('./csv');
 
 const linhas = quebraLinhas(csv);
 const primeiraLinha = linhas.shift();
@@ -15,53 +17,3 @@ console.table(json, ["Filme", "Ano", "Gênero"]);
 
 /////////////////////////////
 
-function quebraLinhas(csv) {
-    let linhas = csv.trim().split('\n');
-    return linhas;
-}
-
-function separaColunas(linha) {
-    let colunas = linha.trim().split(';')
-    return colunas;
-}
-
-function separaConteudo(lines) {
-    const conteudo = [];
-
-    for(let i=0; i<lines.length; i++) {
-        let linha = lines[i];
-        let colunas = separaColunas(linha);
-
-        conteudo.push(colunas);
-    }
-
-    return conteudo;
-}
-
-function constroiObjeto(headers, linhaTabela) {
-    let obj = {};
-    
-    
-    for(let j=0; j<headers.length; j++){
-        let titulo = headers[j];
-        let coluna = linhaTabela[j];
-
-        obj[titulo] = coluna;
-    }
-
-    return obj;
-
-}
-
-function constroiListaDeObjetos(headers, conteudo) {
-    const result = [];
-
-    for(let i=0; i<conteudo.length; i++) {
-        const linhaTabela = conteudo[i];
-        const obj = constroiObjeto(headers, linhaTabela);
-       
-        result.push(obj);
-    }
-
-    return result;
-}
